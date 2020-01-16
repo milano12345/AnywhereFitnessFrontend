a41650e6cedce6589633f5a60119c6dc4a484ecc
milano12345 <dmilano857@gmail.com>
2020-01-16 11:41:19 -0600

    updated landing page styles

--- anywherefitness/src/components/landing.js
+++ anywherefitness/src/components/landing.js
@@ -12,32 +12,76 @@ class Landing extends Component {
               style={{
                 color: "#fff",
                 height: "250px",
-                borderRadius: "80%",
-                marginTop: "30px"
+                marginTop: "30px",
+                borderRadius: "10%",
+                border: "10px solid skyblue"
               }}
               alt={"Wakeboarding"}
             />
           </div>
-          <h2>Daniel Milano</h2>
-          <h3 style={{ color: "grey" }}>Programmer</h3>
-          <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }}></hr>
+          <h4 style={{ color: "black", marginLeft: "5%" }}>Welcome!</h4>
+          <h3 style={{ color: "black", marginLeft: "5%" }}>Dashboard</h3>
+          <hr
+            style={{
+              borderTop: "3px solid #833fb2",
+              width: "74%",
+              marginLeft: "21%"
+            }}
+          ></hr>
           <div className="text-card">
-            <p
-              style={{ textAlign: "left", padding: "16px", color: "darkgray" }}
+            <h5
+              style={{
+                textAlign: "left",
+                paddingLeft: "16px",
+                paddingRight: "16px",
+                paddingTop: "16px",
+                color: "darkgray"
+              }}
             >
-              Daniel is a driven, motivated individual with a passion for
-              learning new technologies. He has been tinkering with websites
-              since the young age of 10. From running bots on Runescape to save
-              precious time, utilizing post data on Firefox to hack Facebook
-              games in the early stages of the website, and to full design
-              customization on his MySpace profile. Daniel now spends his time
-              more productively, learning full stack web development so he can
-              contribute something meaningful to mankind.
+              ***Special News Bulletin!{" "}
+            </h5>
+            <p
+              style={{
+                textAlign: "left",
+                paddingLeft: "16px",
+                paddingRight: "16px",
+                //paddingBottom: "16px",
+                color: "darkgray"
+              }}
+            >
+              -Attention all clients... member dues will be collected on the 5th
+              of next month. Thank you.
+            </p>
+            <p
+              style={{
+                textAlign: "left",
+                paddingLeft: "16px",
+                paddingRight: "16px",
+                paddingBottom: "50px",
+                color: "darkgray"
+              }}
+            >
+              -Attention all clients... member dues will be collected on the 5th
+              of next month. Thank you.
             </p>
           </div>
         </Cell>
         <Cell className="resume-right-col" col={8} style={{ margin: "0px" }}>
           <h2 style={{ margin: "25px" }}>About Me</h2>
+          <p style={{ margin: "25px" }}>
+            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
+            in laying out print, graphic or web designs. The passage is
+            attributed to an unknown typesetter in the 15th century who is
+            thought to have scrambled parts of Malorum for use in a type
+            specimen book.
+          </p>
+          <p style={{ margin: "25px" }}>
+            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
+            in laying out print, graphic or web designs. The passage is
+            attributed to an unknown typesetter in the 15th century who is
+            thought to have scrambled parts of Malorum for use in a type
+            specimen book.
+          </p>
           <p style={{ margin: "25px" }}>
             Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
             in laying out print, graphic or web designs. The passage is
--- anywherefitness/src/index.js
+++ anywherefitness/src/index.js
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import {BrowserRouter as Router} from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom";
+import "react-mdl/extra/material.css";
+import "react-mdl/extra/material.js";
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
